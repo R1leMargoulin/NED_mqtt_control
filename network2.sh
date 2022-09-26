c3165571c426e9d1c06fbd8ccb3ad303aaa8a502
Erwan MARTIN <erwan.martin666@gmail.com>
2022-09-26 23:49:31 +0200

    delete useless file

--- network2.sh
+++
@@ -1,8 +0,0 @@
-echo"entrez l'IP du ned au format X.X.X.X : "
-read IPNED
-
-sudo nmcli con mod niryo-custom ipv4.addresses "$IPNED"/24
-sudo nmcli con mod niryo-custom ipv4.gateway 192.168.0.1
-sudo nmcli con mod niryo-custom ipv4.dns "8.8.8.8"
-sudo nmcli con mod niryo-custom ipv4.method manual
-sudo nmcli con up niryo-custom
