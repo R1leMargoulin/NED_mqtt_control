28d819cb4d81be2401cd7a58a730d8594003c56e
Erwan MARTIN <erwan.martin666@gmail.com>
2022-09-26 23:50:17 +0200

    correcting bootstart

--- bootStart.sh
+++ bootStart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sleep 35
-~/venv/robotics/bin/python ~/pkg_python_self_api/niryo_robot_command.py & roslaunch mlf_mqtt mqtt.launch
+~/venv/robotics/bin/python ~/pkg_python_self_api/niryo_robot_command.py & roslaunch mqtt mqtt.launch
 
